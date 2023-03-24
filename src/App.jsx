0a9d00f99bf770b61fc8484278f2eda4b4969678
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-24 11:36:13 +0200

    panel with cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
 import './styles/reset.css';
 import './styles/App.css';
 import UserForm from './components/UserForm';
+import Card from './components/ui/card/Card';
+import Panel from './components/panel/Panel';
 
 function App() {
   return (
     <div className='container'>
       <h1>Forms Objects</h1>
-      <UserForm />
+      {/* <UserForm /> */}
+      <Card>
+        <Panel></Panel>
+        <Panel></Panel>
+        <Panel></Panel>
+      </Card>
     </div>
   );
 }
