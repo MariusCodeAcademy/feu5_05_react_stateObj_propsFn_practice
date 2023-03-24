ee33e1ee09b577537bc23059d7da4e016a0319c5
Marius CA <mariuskcodeacademy@gmail.com>
2023-03-24 11:44:08 +0200

    PanelList

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import './styles/reset.css';
 import './styles/App.css';
 import UserForm from './components/UserForm';
-import Card from './components/ui/card/Card';
-import Panel from './components/panel/Panel';
+import PanelList from './components/panel/PanelList';
 
 function App() {
   return (
     <div className='container'>
       <h1>Forms Objects</h1>
       {/* <UserForm /> */}
-      <Card>
-        <Panel></Panel>
-        <Panel></Panel>
-        <Panel></Panel>
-      </Card>
+      <PanelList />
     </div>
   );
 }
---
+++ src/components/panel/PanelList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Card from '../ui/card/Card';
+import Panel from './Panel';
+
+function PanelList() {
+  return (
+    <Card>
+      <Panel></Panel>
+      <Panel></Panel>
+      <Panel></Panel>
+    </Card>
+  );
+}
+
+export default PanelList;
